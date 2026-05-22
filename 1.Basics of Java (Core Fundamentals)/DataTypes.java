945d580293aaaa05f250a135e99dc7c420260ac4
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-05-22 14:33:33 +0530

    Add this file

---
+++ 1.Basics of Java (Core Fundamentals)/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int number = 100;
+        float price = 99.99f;
+        char grade = 'A';
+        boolean isJavaEasy = true;
+        String city = "Ujjain";
+
+        System.out.println("Integer: " + number);
+        System.out.println("Float: " + price);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + isJavaEasy);
+        System.out.println("String: " + city);
+    }
+}
