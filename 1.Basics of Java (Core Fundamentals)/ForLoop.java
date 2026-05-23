b1c164a3fbbf7bda3ab60e21045172d6ac3630d0
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-05-23 10:22:04 +0530

    commit changes

---
+++ 1.Basics of Java (Core Fundamentals)/ForLoop.java
@@ -0,0 +1,31 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("For Loop: " + i);
+        }
+    }
+}
+
+
+// public class WhileLoop {
+//     public static void main(String[] args) {
+//         int i = 1;
+
+//         while (i <= 5) {
+//             System.out.println("While Loop: " + i);
+//             i++;
+//         }
+//     }
+// }
+
+
+// public class DoWhileLoop {
+//     public static void main(String[] args) {
+//         int i = 1;
+
+//         do {
+//             System.out.println("Do-While Loop: " + i);
+//             i++;
+//         } while (i <= 5);
+//     }
+// }
