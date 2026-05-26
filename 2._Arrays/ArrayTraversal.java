fcdca4ea0a875c9705f12aca43aa698b964a912e
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-05-26 16:56:26 +0530

    commit changes

---
+++ 2._Arrays/ArrayTraversal.java
@@ -0,0 +1,8 @@
+class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] arr = {5,10,15};
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
