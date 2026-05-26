7d8a4db7a8f4470fc62f94d59808440180f209f2
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-05-26 16:53:49 +0530

    Add this folder

---
+++ 2._Arrays/_SingleDimensionalArray.java
@@ -0,0 +1,8 @@
+class SingleDimensionalArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
