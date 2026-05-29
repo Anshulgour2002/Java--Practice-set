750ee89445b51e4d661ccd699d0b0abc65db378b
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-05-29 23:58:27 +0530

    Add this file

---
+++ 3.Practice-Questions/Riversenum.java
@@ -0,0 +1,19 @@
+public class Riversenum {
+    public static void main(String[] args){
+        int num=17314;
+        int rev=0;
+        int rem;
+
+        while(num!=0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+
+        }
+
+        System.out.println(rev);
+    
+
+        }
+    }
+
