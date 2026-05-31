3b143b9bec9e7c70a972b784315db17d8244a5d3
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-05-31 23:28:41 +0530

    Add this file

---
+++ ObjectOrientedProgramming/Constructor.java
@@ -0,0 +1,22 @@
+public class Constructor {
+    
+int age;
+String name;
+
+
+    public static void main(String[] args) {
+        
+        Constructor cs1=new Constructor();
+        Constructor cs2=new Constructor();
+
+
+        cs1.name="Anshul";
+        cs1.age=24;
+
+
+        cs2.name="Aryan";
+        cs2.age=23;
+
+        
+    }
+}
